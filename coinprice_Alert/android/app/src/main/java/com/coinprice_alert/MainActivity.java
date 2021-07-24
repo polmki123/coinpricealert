1c3eaf8da4ef018eea3e84e130391b4e86ddaf2e
unknown <ok6403@gmail.com>
2021-07-24 22:39:00 +0900

    2021

---
+++ coinprice_Alert/android/app/src/main/java/com/coinprice_alert/MainActivity.java
@@ -0,0 +1,15 @@
+package com.coinprice_alert;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "coinprice_Alert";
+  }
+}
